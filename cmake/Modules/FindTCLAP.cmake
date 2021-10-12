604f21431c5f0b6b2bbc9f7f6e433186de165568
Máté Ferenc Nagy-Egri <mate@streamhpc.com>
2021-10-12 16:01:58 +0200

    Added initial C++ saxpy

---
+++ cmake/Modules/FindTCLAP.cmake
@@ -0,0 +1,28 @@
+# - Find TCLAP
+# Find the TCLAP headers
+#
+# TCLAP_INCLUDE_DIR - where to find the TCLAP headers
+# TCLAP_FOUND       - True if TCLAP is found
+
+if (TCLAP_INCLUDE_DIR)
+  # already in cache, be silent
+  set (TCLAP_FIND_QUIETLY TRUE)
+endif (TCLAP_INCLUDE_DIR)
+
+# find the headers
+find_path (TCLAP_INCLUDE_PATH tclap/CmdLine.h
+  PATHS
+  ${CMAKE_SOURCE_DIR}/include
+  ${CMAKE_INSTALL_PREFIX}/include
+  )
+
+# handle the QUIETLY and REQUIRED arguments and set TCLAP_FOUND to
+# TRUE if all listed variables are TRUE
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args (TCLAP "TCLAP (http://tclap.sourceforge.net/) could not be found. Set TCLAP_INCLUDE_PATH to point to the headers adding '-DTCLAP_INCLUDE_PATH=/path/to/tclap' to the cmake command." TCLAP_INCLUDE_PATH)
+
+if (TCLAP_FOUND)
+  set (TCLAP_INCLUDE_DIR ${TCLAP_INCLUDE_PATH})
+endif (TCLAP_FOUND)
+
+mark_as_advanced(TCLAP_INCLUDE_PATH)
