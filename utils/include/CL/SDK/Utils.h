cc6cbb955689c0275d87503959ac6dc39c45e51c
Ivan Siutsou <ivan@streamhpc.com>
2021-10-22 13:16:33 +0300

    Error code handling

--- utils/include/CL/SDK/Utils.h
+++ utils/include/CL/SDK/Utils.h
@@ -1,12 +1,14 @@
 #pragma once
 
-#include <stdlib.h>
-#include <stdio.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<CL/cl.h>
 
 // reads all the text file contents securely in ANSI C89
 // returns pointer to C-string with file contents
+// can handle streams with no known size and no support for fseek
 // based on https://stackoverflow.com/questions/14002954/ by Nominal Animal
-char * cl_sdk_read_file(char * filename)
+char * cl_utils_read_text_file(char * filename, size_t * length, cl_int * errcode_ret)
 {
     char * data = NULL, * temp;
     size_t size = 0;
@@ -14,20 +16,30 @@ char * cl_sdk_read_file(char * filename)
     size_t n;
     FILE * in;
 
-    #define  READALL_CHUNK 2097152
+    /* Size of each input chunk to be read and allocate for. */
+    #define READALL_CHUNK 2097152
 
     /* File name can not be NULL. */
-    if (filename == NULL)
+    if (errcode_ret == NULL) {
         return NULL;
+    }
+    if (filename == NULL) {
+        *errcode_ret = CL_INVALID_ARG_VALUE;
+        return NULL;
+    }
 
     /* Open file. */
     in = fopen(filename, "r");
-    if (in == NULL)
+    if (in == NULL) {
+        *errcode_ret = CL_INVALID_VALUE;
         return NULL;
+    }
 
     /* A read error already occurred? */
-    if (ferror(in))
+    if (ferror(in)) {
+        *errcode_ret = CL_INVALID_VALUE;
         return NULL;
+    }
 
     while (1) {
         if (used + READALL_CHUNK + 1 > size) {
@@ -36,12 +48,14 @@ char * cl_sdk_read_file(char * filename)
             /* Overflow check. */
             if (size <= used) {
                 free(data);
+                *errcode_ret = CL_OUT_OF_RESOURCES;
                 return NULL;
             }
 
             temp = (char *)realloc(data, size);
             if (temp == NULL) {
                 free(data);
+                *errcode_ret = CL_OUT_OF_HOST_MEMORY;
                 return NULL;
             }
             data = temp;
@@ -56,16 +70,21 @@ char * cl_sdk_read_file(char * filename)
 
     if (ferror(in)) {
         free(data);
+        *errcode_ret = CL_INVALID_VALUE;
         return NULL;
     }
 
     temp = (char *)realloc(data, used + 1);
     if (temp == NULL) {
         free(data);
+        *errcode_ret = CL_OUT_OF_HOST_MEMORY;
         return NULL;
     }
     data = temp;
     data[used] = '\0';
+    if (length != NULL)
+        *length = used;
 
+    *errcode_ret = CL_SUCCESS;
     return data;
 }
