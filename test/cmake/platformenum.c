734f24f3f7214f991cf55d5906ab30f24c8d2562
Máté Ferenc Nagy-Egri <mate@streamhpc.com>
2021-11-16 14:19:51 +0100

    SDK consumption tests

---
+++ test/cmake/platformenum.c
@@ -0,0 +1,20 @@
+// OpenCL includes
+#include <CL/cl.h>
+
+// C standard includes
+#include <stdio.h>
+
+int main()
+{
+    cl_int CL_err = CL_SUCCESS;
+    cl_uint numPlatforms = 0;
+
+    CL_err = clGetPlatformIDs( 0, NULL, &numPlatforms );
+
+    if (CL_err == CL_SUCCESS)
+        printf_s("%u platform(s) found\n", numPlatforms);
+    else
+        printf_s("clGetPlatformIDs(%i)\n", CL_err);
+
+    return 0;
+}
