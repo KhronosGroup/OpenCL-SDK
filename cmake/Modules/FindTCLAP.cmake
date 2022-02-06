597ab9b8c837ead73d42ae6fe3ad6a6c9a486ec5
Máté Ferenc Nagy-Egri <mate@streamhpc.com>
2022-02-06 10:14:51 +0100

    Uniform indendation

--- cmake/Modules/FindTCLAP.cmake
+++ cmake/Modules/FindTCLAP.cmake
@@ -12,14 +12,17 @@ endif (TCLAP_INCLUDE_DIR)
 # find the headers
 find_path (TCLAP_INCLUDE_PATH tclap/CmdLine.h
   PATHS
-  ${CMAKE_SOURCE_DIR}/include
-  ${CMAKE_INSTALL_PREFIX}/include
-  )
+    ${CMAKE_SOURCE_DIR}/include
+    ${CMAKE_INSTALL_PREFIX}/include
+)
 
 # handle the QUIETLY and REQUIRED arguments and set TCLAP_FOUND to
 # TRUE if all listed variables are TRUE
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (TCLAP "TCLAP (http://tclap.sourceforge.net/) could not be found. Set TCLAP_INCLUDE_PATH to point to the headers adding '-DTCLAP_INCLUDE_PATH=/path/to/tclap' to the cmake command." TCLAP_INCLUDE_PATH)
+find_package_handle_standard_args (TCLAP
+  "TCLAP (http://tclap.sourceforge.net/) could not be found. Set TCLAP_INCLUDE_PATH to point to the headers adding '-DTCLAP_INCLUDE_PATH=/path/to/tclap' to the cmake command."
+  TCLAP_INCLUDE_PATH
+)
 
 if (TCLAP_FOUND)
   set (TCLAP_INCLUDE_DIR ${TCLAP_INCLUDE_PATH})
