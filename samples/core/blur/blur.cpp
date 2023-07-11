1347fc6f77c6b6af28f761444e1e6646b4b3d9cd
Bogumil Sapinski <bogumil.sapinski@mobica.com>
2023-07-11 18:02:29 +0200

    formatting

--- samples/core/blur/blur.cpp
+++ samples/core/blur/blur.cpp
@@ -733,7 +733,7 @@ void BlurCppExample::finalize_blur()
     if (input_image.pixel_size != output_image.pixel_size)
     {
         const std::size_t pixels = input_image.width * input_image.height,
-                   pixel_size = output_image.pixel_size;
+                          pixel_size = output_image.pixel_size;
         for (std::size_t i = 1; i < pixels; ++i)
             memcpy(output_image.pixels.data() + pixel_size * i,
                    output_image.pixels.data() + 4 * i, pixel_size);
